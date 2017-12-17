25c25f0f56bc7ff6fb6e865e4ddfe58c2e58587b
Reehan <reehan.lalkhan@gmail.com>
2017-12-17 16:36:00 +0530

    Additional SQL for GL data and

---
+++ DataGenerated/acc_gl_account.sql
@@ -0,0 +1,11 @@
+INSERT INTO acc_gl_account(id,name,parent_id,hierarchy,gl_code,disabled,manual_journal_entries_allowed,account_usage,classification_enum,tag_id,description) VALUES
+('1','Salary Mobilization',NULL,'.','M_Sal','0','1','1','5',13,NULL),
+('2','Salary Loan Servicing',NULL,'.','LS_Sal','0','1','1','5',14,NULL),
+('3','Salary Investment',NULL,'.','I_Sal','0','1','1','5',15,NULL),
+('4','Salary Overheads',NULL,'.','O_Sal','0','1','1','5',16,NULL),
+('5','Salary Provisions',NULL,'.','P_Sal','0','1','1','5',17,NULL),
+('6','Felicitator Charges',NULL,'.','BF_Chrg','0','1','1','5',18,NULL),
+('7','Repairs & Maintanence',NULL,'.','O_R&M','0','1','1','5',17,NULL),
+('8','Meetings',NULL,'.','O_Meet','0','1','1','5',17,NULL),
+('9','Rent',NULL,'.','O_Rent','0','1','1','5',17,NULL),
+('10','Bills',NULL,'.','O_Bills','0','1','1','5',17,NULL);
